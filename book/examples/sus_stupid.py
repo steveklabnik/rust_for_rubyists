1ad517ac7478c7081a7a8376d11e99513e0c13e6
Steve Klabnik <steve@steveklabnik.com>
2012-12-27 00:53:44 -0500

    deleting some stuff

--- book/examples/sus_stupid.py
+++
@@ -1,7 +0,0 @@
-from collections import deque
-
-Gsus = deque(['G', 'C', 'F', 'A'])
-
-for root in list(Gsus):
-    print root, ':', ' '.join(Gsus)
-    Gsus.rotate(1)
--- test.py
+++
@@ -1,3 +0,0 @@
-for i in xrange(0,100):
-    print "I am %d" % i
-
