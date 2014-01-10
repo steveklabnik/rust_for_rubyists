bc1abed08e33e9cef56f37e1318e37ab8f186526
Francisco Lopes <francisco@oblita.com>
2014-01-10 00:40:21 -0200

    Fix for usage of managed boxes
    As per,
    
    https://github.com/mozilla/rust/wiki/Doc-detailed-release-notes#09-january-2014
    
    @ is deprecated.

--- code/07/pointers.rs
+++ code/07/pointers.rs
@@ -1,12 +1,14 @@
+use std::gc::Gc;
+
 fn plus_one(x: &int) -> int {
     *x + 1
 }
 
 fn main() {
-    let x = @10;
+    let x = Gc::new(10);
     let y = ~10;
 
-    println(plus_one(x).to_str());
+    println(plus_one(x.borrow()).to_str());
     println(plus_one(y).to_str());
 }
 
