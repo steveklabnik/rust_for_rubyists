f467cac479ef9e5d2bca9bbfa17e1e0299a197c2
Rene Paulokat <rene@so36.net>
2014-12-31 19:36:17 +0100

    wtf parenthesis...

--- code/01/hello.rs
+++ code/01/hello.rs
@@ -2,7 +2,7 @@ use std::thread::Thread;
 
 fn main() {
     for _ in range(0u, 10) {
-        let _ = Thread::spawn(move ||) {
+        let _ = Thread::spawn(move || {
             let greeting_message = "Hello?";
             println!("{}", greeting_message);
         });
